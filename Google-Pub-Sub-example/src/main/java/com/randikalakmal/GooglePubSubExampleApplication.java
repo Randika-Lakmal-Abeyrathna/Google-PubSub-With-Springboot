c141fe21d0c130e6a88bdd8d3eb7b736bf5ec03b
Randika Lakmal <randila.l.abeyrathna@gmail.com>
2023-01-29 18:29:43 +0530

    Project Init

---
+++ Google-Pub-Sub-example/src/main/java/com/randikalakmal/GooglePubSubExampleApplication.java
@@ -0,0 +1,13 @@
+package com.randikalakmal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GooglePubSubExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GooglePubSubExampleApplication.class, args);
+	}
+
+}
