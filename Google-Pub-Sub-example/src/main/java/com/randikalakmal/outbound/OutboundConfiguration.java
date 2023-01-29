9450ba10f2126fb76f8eb41a7b46db685e851fec
Randika Lakmal <randila.l.abeyrathna@gmail.com>
2023-01-29 19:36:29 +0530

    Pub Sub Inbound and Outbound Configuration completed Added lombok to project

---
+++ Google-Pub-Sub-example/src/main/java/com/randikalakmal/outbound/OutboundConfiguration.java
@@ -0,0 +1,29 @@
+package com.randikalakmal.outbound;
+
+import com.google.cloud.spring.pubsub.core.PubSubTemplate;
+import com.google.cloud.spring.pubsub.integration.outbound.PubSubMessageHandler;
+import com.randikalakmal.config.PubSubConfiguration;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.MessagingGateway;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.messaging.MessageHandler;
+
+@Configuration
+@RequiredArgsConstructor
+public class OutboundConfiguration {
+
+    private final PubSubConfiguration pubSubConfiguration;
+
+    @Bean
+    @ServiceActivator(inputChannel = "pubsubOutputChannel")
+    public MessageHandler messageSender(PubSubTemplate pubSubTemplate){
+        return new PubSubMessageHandler(pubSubTemplate,pubSubConfiguration.getTopic());
+    }
+
+    @MessagingGateway(defaultRequestChannel = "pubsubOutputChannel")
+    public interface PubsubOutboundGateway{
+        void sendToPubsub(String text);
+    }
+}
